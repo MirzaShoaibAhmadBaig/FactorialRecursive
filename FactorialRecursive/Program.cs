c0bcea35fcc472b6fcd9884ddd85dd659f326ad2
armanahmd <92032906+armanahmd@users.noreply.github.com>
2022-03-20 22:15:11 +0500

    Add project files.

---
+++ FactorialRecursive/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FactorialRecursive
+{
+    class Program
+    {
+        static int fastnum = 1;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your Numnber");
+            int UserNum = Int32.Parse(Console.ReadLine());
+
+            int fact= FactorialRecursive(UserNum);
+            Console.WriteLine("Factorial of  {0}  is  {1}", UserNum, fact);
+
+
+        }
+
+        public static int FactorialRecursive(int num )
+        {
+           
+
+            if(num>0)
+            {
+                fastnum = num * FactorialRecursive(num - 1);
+
+            }
+           
+            return fastnum;
+
+
+        }
+    }
+}
